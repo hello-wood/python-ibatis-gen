228a7278cdad85590c45823eea6bcdac166dbdc8
木头 <mutou@lanlanlife.com>
2016-09-28 14:43:19 +0800

    修改包名

--- config/sql_config.py
+++ config/sql_config.py
@@ -12,16 +12,16 @@ gen_config = {
     # 必填
     "table": "xml_gen_table",
     # 必填
-    "product_path": "/Users/linjun/Workspaces/java/xman/xman3-new/",
+    "product_path": "/Users/linjun/Workspaces/java/xman/test/",
     "java_path": "src/main/java/",
     "sql_map_path": "src/main/resources/dal/sqlmap/",
     #默认 [table].xml
     "sql_map_file_name": None,
     # 必填
-    "domain_object_package": "com.zhongan.xman.dataobject",
+    "domain_object_package": "com.test.test.dataobject",
     "domain_object_name": None,
     # 必填
-    "data_access_package": "com.zhongan.xman.dao",
+    "data_access_package": "com.test.test.dao",
     "data_access_interface": None,
     "xml_namespace_prefix": None,
     "is_generate_batch_insert": False,
